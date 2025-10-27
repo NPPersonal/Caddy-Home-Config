78288a4c0903cf8cdc686b4416a05a6720ba0901
tomneo2004 <tomneo2004@gmail.com>
2025-10-27 15:50:13 +0800

    2025/10/27
    - caddy-load-caddyfile.sh and caddy-load.caddyjson.sh now using
    shell script variables which is defined in .env file

--- caddy-load-caddyfile.sh
+++ caddy-load-caddyfile.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-response=$(curl -vsS http://192.168.1.226:2019/load -H "Content-Type: text/caddyfile" --data-binary @configs/Caddyfile)
+# Check if .env file exists
+if [ -f ./.env ]; then
+    # export all variables from .env file
+    set -a
+    source .env
+    set +a
+else
+    echo "ERROR: .env file not found!"
+    exit 1
+fi
+
+response=$(curl -vsS $CADDY_HOST:$CADDY_API_PORT/load -H "Content-Type: $CADDY_CONTENT_TYPE_CADDYFILE" --data-binary @$CADDY_CONFIG_DIR/Caddyfile)
 echo $response
